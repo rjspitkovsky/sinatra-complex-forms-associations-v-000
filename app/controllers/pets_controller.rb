208712729aeb7c861abc1430eab0818b7288b575
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-28 15:28:22 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,13 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
-    @owners = Owner.all 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
-    @owners = Owner.all 
+    @pet = Pet.create(params[:pets])
+    
     redirect to "pets/#{@pet.id}"
   end
 
